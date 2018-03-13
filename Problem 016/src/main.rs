2e46ec99fbe6202dc76a6ccb6b9212c31b74d4ea
Heinmci <dejardin@hotmail.co.uk>
2018-03-13 14:20:32 +0100

    Renamed folders

---
+++ Problem 016/src/main.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    let solution = get_sum_of_digits(1000.);
+    println!("Solution: {}", solution);
+}
+
+fn get_sum_of_digits(power: f64) -> f64 {
+    /*let result: f64 = 2f64.powf(power);
+    result*/
+}
