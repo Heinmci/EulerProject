be653ab688ea0df20ea58cc7dc24c0395738aa51
Heinmci <dejardin@hotmail.co.uk>
2018-03-20 16:21:46 +0100

    Problems 19 and 20

---
+++ Problem 019/src/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+    let days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let solution = sundays_on_first_of_month(&days_in_month);
+    println!("{:?}", solution);
+}
+
+fn sundays_on_first_of_month(days_in_month: &[u8; 12]) -> u8 {
+    let mut current_day = 0;
+    let mut current_month = 0;
+    let mut current_year = 1900;
+    let mut nb_sundays = 0;
+
+    loop {
+        if current_year > 2000 {
+            break;
+        }
+
+        if current_day == 6 && current_year > 1900 {
+            nb_sundays += 1;
+        }
+
+        let nb_days_for_month;
+
+        if current_month == 1 && current_year % 4 == 0 && ((current_year % 100 == 0 && current_year % 400 == 0) || (current_year % 100 != 0)) {
+            nb_days_for_month = days_in_month[current_month] + 1;
+            
+        } else {
+            nb_days_for_month = days_in_month[current_month]
+        }
+
+        current_day += nb_days_for_month % 7;
+        current_day = current_day % 7;
+
+        current_month += 1;
+
+        if current_month > 11 {
+            current_month = 0;
+            current_year += 1;
+        }
+    }
+
+    nb_sundays
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_correct_result_problem_18() {
+        let days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        assert_eq!(sundays_on_first_of_month(&days_in_month), 171);
+    }
+}
