c8029d61dbf3915cdc2925bc4e76c90625ac871a
Heinmci <dejardin@hotmail.co.uk>
2018-03-21 14:47:29 +0100

    Faster is_prime algorithm

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,10 +1,13 @@
 pub fn is_prime(number: u64) -> bool {
     match number {
         0 | 1 => false,
-        2 => true,
+        2 | 3 => true,
         x => {
-            for i in  2..((x as f64).sqrt() as u64) + 1 {
-                if x % i == 0 {
+            if x % 2 == 0 || x % 3 == 0 {
+                return false;
+            } 
+            for i in  (5..((x as f64).sqrt() as u64) + 1).step_by(6) {
+                if x % i == 0 || x % (i + 2) == 0 {
                     return false;
                 }
             }
